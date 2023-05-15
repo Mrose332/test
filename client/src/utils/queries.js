25e9c28496bca5f4b7d324c936c1fe8db23a52eb
Dan Adler <82471930+danbunkr@users.noreply.github.com>
2023-05-15 16:34:45 -0400

    Cleanup

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,15 +15,3 @@ query Comedians {
   }
 }
 `;
-
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
