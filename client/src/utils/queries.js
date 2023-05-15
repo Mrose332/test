184ee1b45c8b6ca5cf0faeebaa974cd30fad9a04
Dan Adler <mrose332@gmail.com>
2023-05-15 16:35:42 -0400

    Test

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,17 @@
 import { gql } from '@apollo/client';
 
-export const COMEDIAN_QUERY = gql`
-query Comedians {
-  comedians {
-    _id
-    first_name
-    last_name
-    shows {
-      city
-      venue
-      date
-      time
-    }
-  }
-}
+	export const COMEDIAN_QUERY = gql`
+	query Comedians {
+	  comedians {
+	    _id
+	    first_name
+	    last_name
+	    shows {
+	      city
+	      venue
+	      date
+	      time
+	    }
+	  }
+	}
 `;
